f3772a277246f1335e054dae1a65233c1dccf3f4
Woojin Yeon <115512265+WOOOOJI@users.noreply.github.com>
2022-12-25 01:06:47 +0900

    Update FileController.java

--- fileupload/src/main/java/com/fileupload/controller/FileController.java
+++ fileupload/src/main/java/com/fileupload/controller/FileController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class FileController {
-	
+	//파일업로드 페이지 매핑
 	@RequestMapping("/")
 	public String filepage() {
 		return "fileUpload";
