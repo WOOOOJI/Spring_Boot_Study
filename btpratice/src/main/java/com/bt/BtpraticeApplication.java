95dfd64b2a29a77a0020df083430034f23c1ff33
WooJin <zathy1225@gmail.com>
2023-01-02 19:41:41 +0900

    Create Project For BootStrap Pratice - Woojin 23.01.02

---
+++ btpratice/src/main/java/com/bt/BtpraticeApplication.java
@@ -0,0 +1,13 @@
+package com.bt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtpraticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtpraticeApplication.class, args);
+	}
+
+}
