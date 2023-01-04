6088b858ffa9d7cbb18b260544cbe9181443552f
WooJin <zathy1225@gmail.com>
2023-01-04 09:12:00 +0900

    javascript test

---
+++ thymeleaf/src/main/resources/static/js/script.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+
+var item = {"name" : "연우진", "age" : 24};
+
+
+window.onload = function(){
+	alert(item.name+""+item.age);
+	document.querySelector("#alink").innerHTML = item.name;
+}
+
